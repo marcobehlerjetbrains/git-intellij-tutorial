c5116be2e8b88f4ac25906fab4bc772f3e3ab767
Marco Behler <marco.behler@jetbrains.com>
2022-07-20 13:27:11 +0200

    Initial commit

---
+++ src/main/java/com/example/gitintellijtutorial/GitIntellijTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitintellijtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIntellijTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIntellijTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitintellijtutorial/GitIntellijTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitintellijtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIntellijTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
